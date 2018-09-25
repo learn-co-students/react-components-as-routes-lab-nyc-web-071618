943ef16749a565c46073cead09faa10245c0bdba
gardiner nardini <gardiner.nardini@gmail.com>
2018-09-25 17:42:39 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { actors } from '../data';
+import v4 from 'uuid'
+
 
 const Actors = () => {
+  
+  let actorsDivs = actors.map((actor)=>{
+    return <div className='actor'
+                key={v4()}>
+                <h1>{actor.name}</h1>
+                <ul>
+                  {actor.movies.map(movie=><li>{movie}</li>)}
+                </ul>
+                </div>
+  })
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actorsDivs}
     </div>
   );
 };
