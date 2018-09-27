db694319e4c68605ca99c8fd6895c75aa38c16d0
trisjones <trisjones04@gmail.com>
2018-09-27 08:39:20 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,17 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actorObj, index)=>{return(
+        <div key={index}>
+          <h2>{actorObj.name}</h2>
+          <ul>
+            {actorObj.movies.map((movie, index)=>{return(
+              <li key={index}>{movie}</li>
+            )})}
+          </ul>
+        </div>
+      )})}
     </div>
   );
 };
