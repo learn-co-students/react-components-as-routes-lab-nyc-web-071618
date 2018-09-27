fdbc27dc8389274b6ae70abc9f83acdd8f46f184
yunglleung1 <yungl.leung1@gmail.com>
2018-09-27 09:38:19 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,11 +2,26 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  let displayActors = () => {
+    // console.log(actors)
+    // debugger
+    let actorList = actors.map((actor) => {
+      return(
+        <div key={actor.name}>
+          name: {actor.name} <br/>
+        movies: {actor.movies} <br/>
+        </div>
+      )
+    })
+    return actorList
+  }
+
   return (
-    <div>
-      {/*{code here}*/}
+    <div>{/*{code here}*/}
+      <h1>Actors Page</h1>
+      {displayActors()}
     </div>
-  );
-};
+  )
+}
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,31 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+
+  const showMovies = () => {
+    // console.log(movies[0].title)
+    // console.log(movies[0].time)
+    // console.log(movies[0].genres)
+    let movieList = movies.map((movie) => {
+      return (
+        <div key={movie.title}>
+          title: {movie.title} <br/>
+          time: {movie.time} <br/>
+          genres: {movie.genres} <br/>
+          <br/>
+        </div>
+      )
+    })
+
+    return movieList
+  }
+
+  // debugger
   return (
-    <div>
-        {/*{code here}*/}
+    <div>{/*{code here}*/}
+      <h1>Movies Page</h1>
+      {showMovies()}
     </div>
   );
 };
